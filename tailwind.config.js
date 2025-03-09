5fceb1d592d5c547d711e70d73f73fb5a805d708
AYAZALAM19 <ayazalamjakirali@gmail.com>
2025-03-09 17:47:18 -0500

    Add custom fonts and update header styles for improved aesthetics and responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
+    colors : {
+      TaupeBrown: '#6E4C41', //header footer and more 
+      white : '#ffff',
+      BrandyPunch : '#D4902A' //use on hover 
+    }
   },
   plugins: [],
 }
